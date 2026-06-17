f79c08a7f35d408937c960eebd3ddb07c3a9ee26
Omar Cerezo <cerezo0408@gmail.com>
2026-06-17 02:50:59 +0000

    Completed exercises

--- app.py
+++ app.py
@@ -1,4 +1,29 @@
 import random
 import math
 
-# your code here
+import random
+
+
+def generate_excuse():
+    who = ["The dog", "My grandma", "His turtle", "My bird"]
+    what = ["ate", "pissed on", "crushed", "broke"]
+    when = [
+        "before the class",
+        "right in time",
+        "when I finished",
+        "during my lunch",
+        "while I was praying",
+    ]
+
+    # Selecciona un elemento aleatorio de cada lista
+    random_who = random.choice(who)
+    random_what = random.choice(what)
+    random_when = random.choice(when)
+
+    # Une las partes para formar la oración
+    excuse = f"{random_who} {random_what} my homework {random_when}."
+    return excuse
+
+
+# Imprime la excusa generada en la consola
+print(generate_excuse())
